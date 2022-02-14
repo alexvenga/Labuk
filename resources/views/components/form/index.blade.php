dd6463a756e91bf7bdc5ae6aeb319a7849bdeed2
alexvenga <bubnov.alexey@gmail.com>
2022-02-14 10:40:49 +0300

    Fixes

--- resources/views/components/form/index.blade.php
+++ resources/views/components/form/index.blade.php
@@ -1,7 +1,10 @@
 @props(['hasFiles'=>false, 'method'=>'GET'])
+@php $method = \Str::upper($method) @endphp
 <form method="{{ $method !== 'GET' ? 'POST' : 'GET' }}"
       @isset($action) action="{{ $action }}" @endisset {!! $hasFiles ? 'enctype="multipart/form-data"' : '' !!} {{ $attributes }}>
-    @csrf
+    @if(!in_array($method,['GET']))
+        @csrf
+    @endif
     @if(!in_array($method,['POST', 'GET']))
         @method($method)
     @endif
