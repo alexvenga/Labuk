a9eea41c3fbd0422a45998ed5f94c964b4c565ed
alexvenga <bubnov.alexey@gmail.com>
2022-03-02 02:10:40 +0300

    Select fix

--- resources/views/components/form/input/select.blade.php
+++ resources/views/components/form/input/select.blade.php
@@ -3,7 +3,7 @@
 ])
 <select {{ $attributes->merge([
     'class' => 'block w-full shadow-sm text-base focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md focus:outline-none',
-]) }}/>
+]) }}>
     @if ($placeholder !== null)
         <option value="{{ null }}">{{ is_bool($placeholder) ? '' : $placeholder }}</option>
     @endif
