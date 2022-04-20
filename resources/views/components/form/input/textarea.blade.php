dcb9b6ec1f11a26dedf38a2971b549623a018f51
alexvenga <bubnov.alexey@gmail.com>
2022-04-20 03:32:25 +0300

    Textarea  !!}

--- resources/views/components/form/input/textarea.blade.php
+++ resources/views/components/form/input/textarea.blade.php
@@ -2,4 +2,4 @@
     'class' => 'block w-full shadow-sm text-base focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md',
     'type'  => 'text',
     'rows'=>'3',
-])->except(['value']) }}>@isset($value){{ $value }}@endisset</textarea>
+])->except(['value']) }}>@isset($value){!! $value !!}@endisset</textarea>
