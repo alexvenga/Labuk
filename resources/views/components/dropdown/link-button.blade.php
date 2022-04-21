d4bab9752f44111e5ae46cd896f8445c0b215605
alexvenga <bubnov.alexey@gmail.com>
2022-04-21 17:04:58 +0300

    Dropdown button

--- resources/views/components/dropdown/link-button.blade.php
+++ resources/views/components/dropdown/link-button.blade.php
@@ -2,7 +2,7 @@
     'active' => false,
 ])
 <button tabindex="-1" {{ $attributes->merge([
-    'class' => ($active ? 'bg-gray-200 text-gray-900' : 'text-gray-700 hover:bg-gray-200 hover:text-gray-900').' block px-4 py-2 text-sm',
+    'class' => ($active ? 'bg-gray-200 text-gray-900' : 'text-gray-700 hover:bg-gray-200 hover:text-gray-900').' block px-4 py-2 text-sm w-full',
     'type'  => 'button',
 ]) }}>
     {{ $slot }}
