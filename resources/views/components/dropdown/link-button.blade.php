922614078bc49946c0fc426628743d2c8634384b
alexvenga <bubnov.alexey@gmail.com>
2022-04-21 17:01:49 +0300

    Dropdown button

---
+++ resources/views/components/dropdown/link-button.blade.php
@@ -0,0 +1,9 @@
+@props([
+    'active' => false,
+])
+<button tabindex="-1" {{ $attributes->merge([
+    'class' => ($active ? 'bg-gray-200 text-gray-900' : 'text-gray-700 hover:bg-gray-200 hover:text-gray-900').' block px-4 py-2 text-sm',
+    'type'  => 'button',
+]) }}>
+    {{ $slot }}
+</button>
